82d50ff37fce06d1918e6cf31dafadfd3ee90af9
Oleksii <Alex13867777@gmail.com>
2021-10-14 10:22:25 +0300

    Updates

--- static/js/main.cdbb8a80.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var c=n(1),o=n.n(c),r=n(3),s=n.n(r),a=(n(8),n(9),n(0));var i=function(){return Object(a.jsx)("div",{className:"App",children:"Let's start to learn React"})};s.a.render(Object(a.jsx)(o.a.StrictMode,{children:Object(a.jsx)(i,{})}),document.getElementById("root"))},8:function(t,e,n){},9:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.cdbb8a80.chunk.js.map
